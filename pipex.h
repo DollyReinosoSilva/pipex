69846650f6f83a4227da7f663b689fdb1cc7d882
Dolly Reinoso <dollyreinososilva@gmail.com>
2025-06-12 19:37:18 +0200

    Add files via upload

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: dreinoso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 03:03:16 by dreinoso          #+#    #+#             */
-/*   Updated: 2025/06/07 23:34:57 by dreinoso         ###   ########.fr       */
+/*   Updated: 2025/06/12 19:34:44 by dreinoso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,8 @@
 # include "libft/libft.h"
 
 void	error(char *cmd, int not_found);
-//char	*find_path(char *cmd, char **envp);
 char	**get_paths(char **envp);
 char	*find_path(char *cmd, char **envp);
 void	free_split(char **split);
 void	execute(char *argv, char **envp);
-//int		get_next_line(char **line);
 #endif
